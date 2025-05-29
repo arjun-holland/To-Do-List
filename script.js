80a69b4fe50693143ac287c9c8a55f19ba5a94b2
Mallikarjuna <spiderman05116@gmail.com>
2025-05-30 00:51:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,31 @@ function addTask() {
     saveData();
 }
 
+
+/*
+listContainer.addEventListener("click", function(e) { ... })
+This line attaches a click event to listContainer.
+Whenever anything inside listContainer is clicked, the function runs.
+e is the event object, which contains info about what was clicked.
+
+e.target
+e.target is the actual element that was clicked (for example: an <li> or <span> inside the list).
+
+What is e.target.classList.toggle("checked")?
+🔹 e.target
+This is the element that was clicked.
+🔹 classList
+This gives access to all the CSS classes applied to that element.
+🔹 .toggle("checked")
+This means:
+If the element already has the class checked, it will remove it.
+If it doesn’t have the class checked, it will add it.
+
+parentElement
+This is the HTML element that wraps around the clicked element. i.e <li>
+*/
+
+
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
